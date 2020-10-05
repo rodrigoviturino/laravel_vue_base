19c2304f5e2e2e7f4e593d35fc98ee0bdf72cdaa
Rodrigo Viturino <Viturino_souza@outlook.com>
2020-10-05 00:56:32 -0300

    criando pagina view de artigos

---
+++ resources/views/admin/artigos/index.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.app')
+
+@section('content')
+
+  <pagina tamanho="8">
+    <painel titulo="Dashboard">
+      Conteudo Painel
+
+      <div class="row">
+        <div class="col-md-4">
+          <caixa qtd="22" titulo="Artigos" url="#teste" cor="gray" icone="ion ion-person-add"></caixa>
+        </div>
+        <div class="col-md-4">
+          <caixa qtd="80" titulo="Games" url="#" cor="purple" icone="ion-ios-game-controller-b-outline"></caixa>
+        </div>
+        <div class="col-md-4">
+          <caixa qtd="268" titulo="Posts" url="#" cor="blue" icone="ion-pound"></caixa>
+        </div>
+      </div>
+
+    </painel>
+  </pagina>
+@endsection
