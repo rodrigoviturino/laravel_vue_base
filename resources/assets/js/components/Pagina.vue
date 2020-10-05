32fa364885d17788c5838a0f0dc31c6c4fc09a97
Rodrigo Viturino <Viturino_souza@outlook.com>
2020-10-05 00:52:53 -0300

    desenvolvido componente de Pagina.vue e Caixa.vue e aplicando na pagina home do laravel

---
+++ resources/assets/js/components/Pagina.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div v-bind:class="defineTamanho">
+        <slot></slot>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["tamanho"],
+  computed: {
+    defineTamanho: function() {
+      if (this.tamanho >= 12) {
+        return "col-md-12";
+      } else if (this.tamanho <= 2) {
+        return "col-md-2 col-md-offset-5";
+      } else if (this.tamanho % 2 == 0) {
+        return (
+          "col-md-" + this.tamanho + " col-md-offset-" + (12 - this.tamanho) / 2
+        );
+      } else {
+        return (
+          "col-md-" +
+          (Number(this.tamanho) + 1) +
+          " col-md-offset-" +
+          (12 - (Number(this.tamanho) + 1)) / 2
+        );
+      }
+    },
+  },
+};
+</script>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,27 +2,22 @@
 
 @section('content')
 
-  <div class="container">
-    <div class="row">
-      <div class="col-md-10 col-md-offset-1">
-        <painel titulo="Dashboard">
-          Conteudo Painel
+  <pagina tamanho="8">
+    <painel titulo="Dashboard">
+      Conteudo Painel
 
-          <div class="row">
-            <div class="col-md-4">
-              <painel titulo="conteudo 1">Lorem Ipsum 1</painel>
-            </div>
-            <div class="col-md-4">
-              <painel titulo="conteudo 1">Lorem Ipsum 2</painel>
-            </div>
-            <div class="col-md-4">
-              <painel titulo="conteudo 1">Lorem Ipsum 3</painel>
-            </div>
-          </div>
-
-        </painel>
+      <div class="row">
+        <div class="col-md-4">
+          <caixa qtd="22" titulo="Artigos" url="#teste" cor="gray" icone="ion ion-person-add"></caixa>
+        </div>
+        <div class="col-md-4">
+          <caixa qtd="80" titulo="Games" url="#" cor="purple" icone="ion-ios-game-controller-b-outline"></caixa>
+        </div>
+        <div class="col-md-4">
+          <caixa qtd="268" titulo="Posts" url="#" cor="blue" icone="ion-pound"></caixa>
+        </div>
       </div>
-    </div>
-  </div>
 
+    </painel>
+  </pagina>
 @endsection
